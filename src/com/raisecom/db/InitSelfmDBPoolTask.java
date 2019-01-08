089affc6fb1e642430a1f0a90b5694489d9928c0
liujs <liujiashuo0811@163.com>
2019-01-08 15:43:22 +0800

    导出文件

--- src/com/raisecom/db/InitSelfmDBPoolTask.java
+++ src/com/raisecom/db/InitSelfmDBPoolTask.java
@@ -2,20 +2,10 @@ package com.raisecom.db;
 
 import com.raisecom.common.logging.LogFactory;
 import com.raisecom.common.logging.Logger;
-import com.raisecom.nms.platform.client.ResourceManager;
 import com.raisecom.nms.platform.cnet.ObjService;
-import com.raisecom.nms.selfm.util.SelfmUtils;
 import com.raisecom.nms.util.DBConnectionManager;
 import com.raisecom.util.XMLFileHelperUtil;
 
-import javax.swing.*;
-import java.awt.*;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Iterator;
-import java.util.Vector;
 
 /**
  * Created by ligy-008494 on 2018/11/14.
